b2853473ed2c35391494c8f983ac7551285b3307
Rafa <58285574+JsRphlMrtnz@users.noreply.github.com>
2024-03-28 20:40:57 +0800

    feat: initial asm implementation
    Co-Authored-By: Kovie <dev@thekovie.com>

--- LBYARCH_IPProject/asmfunc.asm
+++ LBYARCH_IPProject/asmfunc.asm
@@ -1,16 +1,38 @@
 section .data
-msg db "Hello World", 13, 10, 0
 
 section .text 
 bits 64
 default rel
 
-global asmhello
-extern printf
+global saxpy_asm
 
-asmhello:
-	SUB RSP, 8*5
-	LEA RCX, [msg]
-	call printf
-	add RSP, 8*5
+saxpy_asm:
+	PUSH RSI
+	PUSH RBP
+	XOR RAX, RAX
+	; xmm0 a
+	LEA R10, [RDX] ; x
+	LEA R11, [R8] ; y
+	LEA R12, [R9] ; z
+	MOV R13, [RBP+32] ; size
+	; loop until size is 0
+LP: 
+	MOVSS xmm1, [R10]
+	MULSS xmm1, xmm0 ; a * x
+
+	ADDSS xmm1, [R11] ; a*x + y
+	MOVSS [R12], xmm1 ; store to z
+
+	CMP R13, 0
+	JE END
+	DEC R13
+	INC R10
+	INC R11
+	INC R12
+	MOV RCX, R13
+
+
+END:
+	POP RBP
+	POP RSI
 	ret
--- LBYARCH_IPProject/main.c
+++ LBYARCH_IPProject/main.c
@@ -7,7 +7,7 @@
 #define size2 1 << 24
 #define size3 1 << 30
 
-extern void asmhello();
+extern void saxpy_asm(float a, float *x, float *y, float *z, unsigned int size);
 
 void fill_vector(float vec[], unsigned int size) {
 	unsigned int i;
@@ -35,8 +35,8 @@ void print_vec(float vec[], unsigned int size) {
 }
 
 int main() {
-	//asmhello();
 	srand(time(NULL));
+
 	// declare max size for all vectors
 	unsigned int currSize = size2;
 	float *x = (float*)malloc(sizeof(float) * currSize);
@@ -46,8 +46,9 @@ int main() {
 	fill_vector(x, currSize);
 	fill_vector(y, currSize);
 	double timesC = 0;
+	double timesASM = 0;
 	unsigned int i;
-	for (i = 0; i < 30; i++) {
+	/*for (i = 0; i < 30; i++) {
 		clock_t start = clock();
 		saxpy(a, x, y, z, currSize);
 		clock_t end = clock();
@@ -57,7 +58,19 @@ int main() {
 	}
 	timesC /= 30.0;
 	timesC *= 1000;
-	printf("average time for C: %fms", timesC);
+	printf("average time for C: %fms", timesC);*/
+
+	for (i = 0; i < 30; i++) {
+		clock_t start = clock();
+		saxpy_asm(a, x, y, z, currSize);
+		clock_t end = clock();
+		timesASM += (double)(end - start) / CLOCKS_PER_SEC;
+		printf("\nz\n");
+		print_vec(z, currSize);
+	}
+	timesASM /= 30.0;
+	timesASM *= 1000;
+	printf("average time for ASM: %fms", timesASM);
 
 	free(x);
 	free(y);
